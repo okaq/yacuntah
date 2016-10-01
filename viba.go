2e39c158c540faf2bb71b515aac342b67425e8e4
AQ <aq@okaq.com>
2016-10-01 17:04:40 -0400

    Atomic id counter increment

--- viba.go
+++ viba.go
@@ -7,10 +7,16 @@ package main
 import (
     "fmt"
     "net/http"
+    "strconv"
+    "sync/atomic"
 )
 
 const (
     INDEX = "viba.html"
+)
+
+var (
+    ID uint64 // uinque id generator
 )
 
 func VibaHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,13 +28,20 @@ func JsonHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
 }
 
-func AnkhHandler() {
+func AnkhHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
     // atomic int64 counter for user ids
     // server is id:0, admin is id:1, player aq is id:2
+    new_id := atomic.AddUint64(&ID, 1)
+    w.Header().Set("Content-Type", "application/json")
+    s0 := strconv.FormatUint(new_id, 10)
+    b0 := []byte(s0)
+    w.Write(b0)
 }
 
 func main() {
     fmt.Println("starting viba web server on http://localhost:8080");
+    ID = 2
     http.HandleFunc("/", VibaHandler)
     http.HandleFunc("/json", JsonHandler)
     http.HandleFunc("/a", AnkhHandler)
