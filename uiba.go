41b3d0e0d2930e28baafda9912c5597a2fad3d5c
AQ <aq@okaq.com>
2016-10-12 08:27:31 -0400

    Noto font async

--- uiba.go
+++ uiba.go
@@ -10,13 +10,13 @@ const (
     INDEX = "uiba.html"
 )
 
-func UibaHandler() {
+func UibaHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
     http.ServeFile(w, r, INDEX)
 }
 
 func main() {
-    fmt.Println("starting viba web server on http://localhost:8080");
-    http.HandleFunc("/", VibaHandler)
+    fmt.Println("starting uiba web server on http://localhost:8080");
+    http.HandleFunc("/", UibaHandler)
     http.ListenAndServe(":8080", nil)
 }
