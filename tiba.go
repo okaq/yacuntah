7bb24a57bf36607f60c61a3ad3e343f9839d1ae7
AQ <aq@okaq.com>
2016-11-02 08:55:37 -0400

    Noto fluid max rand integer

--- tiba.go
+++ tiba.go
@@ -15,8 +15,14 @@ func TibaHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, INDEX)
 }
 
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    w.Write([]byte("ready one"))
+}
+
 func main() {
     fmt.Println("starting tiba web server on http://localhost:8080")
     http.HandleFunc("/", TibaHandler)
+    http.HandleFunc("/u", UserHandler)
     http.ListenAndServe(":8080", nil)
 }
