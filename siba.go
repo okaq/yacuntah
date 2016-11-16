39365e3aa0c2d3ba8dc6f846cc5ad475afa4abf1
AQ <aq@okaq.com>
2016-11-15 20:44:18 -0500

    Peer net id

--- siba.go
+++ siba.go
@@ -10,13 +10,21 @@ const (
     INDEX = "siba.html"
 )
 
+// cache list of peer quids
+// maintain broker for any peer net
+
 func SibaHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
     http.ServeFile(w, r, INDEX)
 }
 
+func QuidHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+}
+
 func main() {
     fmt.Println("starting siba web server on localhost:8080")
     http.HandleFunc("/", SibaHandler)
+    http.HandleFunc("/a", QuidHandler)
     http.ListenAndServe(":8080", nil)
 }
